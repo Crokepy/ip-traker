7034561af778586a20f849008bb4662411bcfd82
Croque <sdeing1@gmail.com>
2021-02-25 11:48:41 -0400

    xd

--- js/fun.js
+++ js/fun.js
@@ -2,9 +2,11 @@ export async function pet(ip) {
   let req = await fetch(
     `https://geo.ipify.org/api/v1?apiKey=at_jUtsrpq8rjjAaKYw5AYF2O4HLvBzc&ipAddress=${ip}`
   );
+
   let data = await req.json();
   const ubi = data.location.region;
   const timeZone = data.location.timezone;
   const compañia = data.isp;
   console.log(compañia);
+  return ubi, timeZone, compañia;
 }
